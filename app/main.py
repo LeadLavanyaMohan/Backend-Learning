e6aace321cc9fd6c673c314eab1c988984d32de4
lavanya <lavanyamohanrj2@gmail.com>
2025-12-29 22:17:53 +0530

    create post featues completed

--- app/main.py
+++ app/main.py
@@ -73,7 +73,10 @@ def create_post(post: Post,db: Session = Depends(get_db) ):
     # new_post = cursor.fetchone()
 
     # conn.commit()
-    new_post= models.Post(title=post.title,content=post.content,published=post.published)
+    
+    # new_post= models.Post(title=post.title,content=post.content,published=post.published)
+
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
