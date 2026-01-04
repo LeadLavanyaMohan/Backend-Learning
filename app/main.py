2068d89d6e574c6ae479572c55333912f8fc19c6
lavanya <lavanyamohanrj2@gmail.com>
2026-01-04 21:55:10 +0530

    User registration module created sucessfully

--- app/main.py
+++ app/main.py
@@ -118,3 +118,12 @@ def update_Post(id : int, updated_post : schemas.PostCreate,db: Session = Depend
     
     return post_query.first()
 
+@app.post("/users",status_code= status.HTTP_201_CREATED,response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate,db : Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
