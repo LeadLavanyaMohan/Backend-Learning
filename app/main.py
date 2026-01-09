13f798c89c079dfefda7e8a1f73feb44e7fa70ab
lavanya <lavanyamohanrj2@gmail.com>
2026-01-09 22:16:38 +0530

    working on JWT Authentication feature

--- app/main.py
+++ app/main.py
@@ -55,95 +55,95 @@ app.include_router(user.router)
 def health():
     return {"status": "connected"}
 
-@app.get("/posts",response_model=List[schemas.Post])
-def get_post(db: Session = Depends(get_db)):
-    # cursor.execute(""" SELECT * FROM posts;""")
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return posts
+# @app.get("/posts",response_model=List[schemas.Post])
+# def get_post(db: Session = Depends(get_db)):
+#     # cursor.execute(""" SELECT * FROM posts;""")
+#     # posts = cursor.fetchall()
+#     posts = db.query(models.Post).all()
+#     return posts
 
 
-@app.post("/createPosts",status_code= status.HTTP_201_CREATED,response_model = schemas.Post)
-def create_post(post: schemas.PostCreate,db: Session = Depends(get_db) ):
-    # cursor.execute("""INSERT INTO posts (title,content,published) VALUES(%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
-    # new_post = cursor.fetchone()
+# @app.post("/createPosts",status_code= status.HTTP_201_CREATED,response_model = schemas.Post)
+# def create_post(post: schemas.PostCreate,db: Session = Depends(get_db) ):
+#     # cursor.execute("""INSERT INTO posts (title,content,published) VALUES(%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
+#     # new_post = cursor.fetchone()
 
-    # conn.commit()
+#     # conn.commit()
     
-    # new_post= models.Post(title=post.title,content=post.content,published=post.published)
+#     # new_post= models.Post(title=post.title,content=post.content,published=post.published)
 
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
+#     new_post = models.Post(**post.dict())
+#     db.add(new_post)
+#     db.commit()
+#     db.refresh(new_post)
+#     return new_post
 
-@app.get("/{id}",status_code = status.HTTP_404_NOT_FOUND,response_model = schemas.Post)
-def get_Post_By_Id( id : int,db: Session = Depends(get_db)):
-    # cursor.execute(""" SELECT * FROM posts WHERE id = %s""",(str(id)))
-    # test_post = cursor.fetchone()
+# @app.get("/{id}",status_code = status.HTTP_404_NOT_FOUND,response_model = schemas.Post)
+# def get_Post_By_Id( id : int,db: Session = Depends(get_db)):
+#     # cursor.execute(""" SELECT * FROM posts WHERE id = %s""",(str(id)))
+#     # test_post = cursor.fetchone()
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    print(post)
+#     post = db.query(models.Post).filter(models.Post.id == id).first()
+#     print(post)
 
-    if not post:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail={ "message" : f"Post with a given id {id} is found"})
-    else:
-        return post
+#     if not post:
+#         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail={ "message" : f"Post with a given id {id} is found"})
+#     else:
+#         return post
         
-@app.delete("/{id}",status_code= status.HTTP_204_NO_CONTENT)
-def delete_post(id :int,db: Session = Depends(get_db)):
-    # cursor.execute(""" DELETE FROM posts WHERE id = %s returning * """,(str(id)))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
+# @app.delete("/{id}",status_code= status.HTTP_204_NO_CONTENT)
+# def delete_post(id :int,db: Session = Depends(get_db)):
+#     # cursor.execute(""" DELETE FROM posts WHERE id = %s returning * """,(str(id)))
+#     # deleted_post = cursor.fetchone()
+#     # conn.commit()
 
-    post = db.query(models.Post).filter(models.Post.id == id)
+#     post = db.query(models.Post).filter(models.Post.id == id)
 
-    if post.first() == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail={ "message" : f"Post with a given id {id} is found"})
+#     if post.first() == None:
+#         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail={ "message" : f"Post with a given id {id} is found"})
     
-    post.delete(synchronize_session = False)
-    db.commit()
+#     post.delete(synchronize_session = False)
+#     db.commit()
     
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+#     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}",response_model = schemas.Post)
-def update_Post(id : int, updated_post : schemas.PostCreate,db: Session = Depends(get_db)):
-    # cursor.execute(""" UPDATE posts SET title = %s,content = %s,published = %s  WHERE id = %s RETURNING *""",(post.title,post.content,post.published,(str(id))))
-    # updated_Post = cursor.fetchone()
-    # conn.commit()
+# @app.put("/posts/{id}",response_model = schemas.Post)
+# def update_Post(id : int, updated_post : schemas.PostCreate,db: Session = Depends(get_db)):
+#     # cursor.execute(""" UPDATE posts SET title = %s,content = %s,published = %s  WHERE id = %s RETURNING *""",(post.title,post.content,post.published,(str(id))))
+#     # updated_Post = cursor.fetchone()
+#     # conn.commit()
 
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    if post == None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail={ "message" : f"Post with a given id {id} is found"})
+#     post_query = db.query(models.Post).filter(models.Post.id == id)
+#     post = post_query.first()
+#     if post == None:
+#         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail={ "message" : f"Post with a given id {id} is found"})
     
-    post_query.update(updated_post.dict(),synchronize_session = False)
-    db.commit()
+#     post_query.update(updated_post.dict(),synchronize_session = False)
+#     db.commit()
     
-    return post_query.first()
+#     return post_query.first()
 
-@app.post("/users",status_code= status.HTTP_201_CREATED,response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate,db : Session = Depends(get_db)):
+# @app.post("/users",status_code= status.HTTP_201_CREATED,response_model=schemas.UserOut)
+# def create_user(user: schemas.UserCreate,db : Session = Depends(get_db)):
 
-    #hash the password - user.password
-    # hashed_pwd = pwd_context.hash(user.password)
-    # user.password = hashed_pwd
-    user.password = utils.hash_password(user.password)
+#     #hash the password - user.password
+#     # hashed_pwd = pwd_context.hash(user.password)
+#     # user.password = hashed_pwd
+#     user.password = utils.hash_password(user.password)
 
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+#     new_user = models.User(**user.dict())
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
 
-    return new_user
+#     return new_user
 
 
-@app.get("/users/{id}",status_code = status.HTTP_404_NOT_FOUND,response_model=schemas.UserOut)
-def get_user(id:int,db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
+# @app.get("/users/{id}",status_code = status.HTTP_404_NOT_FOUND,response_model=schemas.UserOut)
+# def get_user(id:int,db: Session = Depends(get_db)):
+#     user = db.query(models.User).filter(models.User.id == id).first()
 
-    if not user:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail ={ "message" : f"User with a given id {id} is found"} )
+#     if not user:
+#         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail ={ "message" : f"User with a given id {id} is found"} )
     
-    return user 
+#     return user 
