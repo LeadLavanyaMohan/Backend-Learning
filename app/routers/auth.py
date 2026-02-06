fc2f3e08590b4c9abc8625eaa9be06c6a1852b02
lavanya <lavanyamohanrj2@gmail.com>
2026-02-06 23:21:50 +0530

    bug fixed in login module

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,9 +5,9 @@ from .. import database,schemas,models,utils
 router = APIRouter(tags=['Authentication'])
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin,db :  Session = Depends(database.get_db())):
+def login(user_credentials: schemas.UserLogin,db :  Session = Depends(database.get_db)):
 
-    user = db.query(models.User).filter(user.email == user_credentials.email).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
 
     if not user:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail ={ "message" : f"Invalid Credentials"} )
